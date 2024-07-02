4b55f0204477c8074a9f71cdcee0916848d8802f
Waptech <abdulfataiyakub07@gmail.com>
2024-07-02 08:31:41 +0100

    Added a getIP()  method to HelloView that retrieves the client's external IP address using urllib and regex.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from urllib.request import urlopen
+import re as r
 
 # Create your views here.
+
+class HelloView(APIView):
+    def getIP(self):
+        try:
+            d = str(urlopen('http://checkip.dyndns.com/').read())
+            ip_address = r.compile(r'Address: (\d+\.\d+\.\d+\.\d+)').search(d).group(1)
+        except Exception as e:
+            ip_address = None
+        return ip_address    
